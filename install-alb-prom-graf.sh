01aa8b1720d1e84e76ac7db24c4bafe9a08c61e6
Bash-mocart <bashirsuleiman77@gmail.com>
2022-05-16 04:03:46 +0100

    fixed grafana

--- install-alb-prom-graf.sh
+++ install-alb-prom-graf.sh
@@ -8,4 +8,6 @@ helm repo add prometheus-community https://prometheus-community.github.io/helm-c
 helm upgrade -i prometheus prometheus-community/prometheus \
   --namespace prometheus \
   --set alertmanager.persistentVolume.storageClass="gp2",server.persistentVolume.storageClass="gp2"
-kubectl apply -f grafana.yaml -n grafana
+helm repo add grafana https://grafana.github.io/helm-charts
+helm update
+helm upgrade -i grafana grafana/grafana --namespace grafana
